fde016ae44ce353eadb214fd4e0ea1c370d531d4
Christer Wendel <christer.wendel@altrusoft.se>
2015-06-22 22:16:29 +0200

    Limiting repository configuration to i386 and amd64

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,10 +4,12 @@
 class elasticsearch::install {
   include aptrepo
 
-  aptrepo::addrepo { 'elasticsearch':
-    location => 'http://packages.elasticsearch.org/elasticsearch/1.6/debian/',
-    key      => 'http://packages.elasticsearch.org/GPG-KEY-elasticsearch',
-  } -> Package['elasticsearch']
+  if $::architecture == 'amd64' or $::architecture == 'i386' {
+    aptrepo::addrepo { 'elasticsearch':
+      location => 'http://packages.elasticsearch.org/elasticsearch/1.6/debian/',
+      key      => 'http://packages.elasticsearch.org/GPG-KEY-elasticsearch',
+      } -> Package['elasticsearch']
+  }
 
   package { 'elasticsearch':
     ensure => $elasticsearch::version,
--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -4,13 +4,40 @@ describe 'elasticsearch', :type => :class do
   let(:title) { 'elasticsearch::install' }
 
   context 'with default parameters' do
-    it { should contain_aptrepo__addrepo('elasticsearch')
+    it { should_not contain_aptrepo__addrepo('elasticsearch')
         .with_location('http://packages.elasticsearch.org/elasticsearch/1.6/debian/')
         .with_key('http://packages.elasticsearch.org/GPG-KEY-elasticsearch')
     }
 
     it { should contain_package('elasticsearch')
         .with_ensure('present')
+    }
+  end
+
+  context 'architecture => amd64' do
+    let(:facts) { {:architecture => 'amd64'} }
+
+    it { should contain_aptrepo__addrepo('elasticsearch')
+        .with_location('http://packages.elasticsearch.org/elasticsearch/1.6/debian/')
+        .with_key('http://packages.elasticsearch.org/GPG-KEY-elasticsearch')
+    }
+  end
+
+  context 'architecture => i386' do
+    let(:facts) { {:architecture => 'i386'} }
+
+    it { should contain_aptrepo__addrepo('elasticsearch')
+        .with_location('http://packages.elasticsearch.org/elasticsearch/1.6/debian/')
+        .with_key('http://packages.elasticsearch.org/GPG-KEY-elasticsearch')
+    }
+  end
+
+  context 'architecture => armv7l' do
+    let(:facts) { {:architecture => 'amdv7l'} }
+
+    it { should_not contain_aptrepo__addrepo('elasticsearch')
+        .with_location('http://packages.elasticsearch.org/elasticsearch/1.6/debian/')
+        .with_key('http://packages.elasticsearch.org/GPG-KEY-elasticsearch')
     }
   end
 
